49fc1b2bb2ca4196331c97bb5891c86e5d733bfb
Stephen Wakely <fungus.humungus@gmail.com>
2026-03-17 23:10:08 +0100

    Add cache hit and miss telemetry

--- pkg/aggregator/aggregator.go
+++ pkg/aggregator/aggregator.go
@@ -165,6 +165,8 @@ var (
 	tlmDogstatsdFilteredMetrics = telemetry.NewSimpleCounter("aggregator", "dogstatsd_filtered_metrics", "How many metrics were filtered in the time samplers")
 	tlmChecksFilteredMetrics    = telemetry.NewSimpleCounter("aggregator", "checks_filtered_metrics", "How many metrics were filtered in the check samplers")
 	tlmFilteredTags             = telemetry.NewSimpleCounter("aggregator", "filtered_tags", "How many tags were filtered from a metric sample")
+	tlmFilteredTagsCacheHit     = telemetry.NewSimpleCounter("aggregator", "filtered_tags_cache_hit", "How many times we hit the cache on filtering tags")
+	tlmFilteredTagsCacheMiss    = telemetry.NewSimpleCounter("aggregator", "filtered_tags_cache_miss", "How many times we missed the cache on filtering tags")
 	tlmChecksContexts           = telemetry.NewGauge("aggregator", "checks_contexts",
 		[]string{"shard"}, "Count the number of checks contexts in the check aggregator")
 	tlmChecksContextsByMtype = telemetry.NewGauge("aggregator", "checks_contexts_by_mtype",
